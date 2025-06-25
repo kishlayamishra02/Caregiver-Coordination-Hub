67b1dc8e03d70c2ed3a747960d7073fc064316f0
Arjit74 <sharmaarjit886@gmail.com>
2025-06-25 17:27:50 +0530

    feat: update messaging service worker and add Firebase config

--- caregiver-hub/public/firebase-messaging-sw.js
+++ caregiver-hub/public/firebase-messaging-sw.js
@@ -2,16 +2,7 @@
 
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
-
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
-};
+importScripts('/firebase-sw-config.js');
 
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
---
+++ caregiver-hub/public/firebase-sw-config.js
@@ -0,0 +1,10 @@
+self.FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDYXV-5RNkaATCRk0kZfLQ3NMmPtGw0v8I",
+    authDomain: "unity-link-26935.firebaseapp.com",
+    projectId: "unity-link-26935",
+    storageBucket: "unity-link-26935.appspot.com",
+    messagingSenderId: "334131837545",
+    appId: "1:334131837545:web:5ffb212835645bc1d500f8",
+    measurementId: "G-Z1S2KZTC7Y"
+  };
+  
