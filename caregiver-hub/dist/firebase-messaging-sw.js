a37e2a5052c3f0b8678853a067843464916a9bc8
Arjit74 <sharmaarjit886@gmail.com>
2025-06-26 11:35:04 +0530

    Update firebase messaging service worker and Netlify config

--- caregiver-hub/dist/firebase-messaging-sw.js
+++ caregiver-hub/dist/firebase-messaging-sw.js
@@ -4,14 +4,15 @@ importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyAxh0tIjlxGm1CCtmlhwCyagYQ4MNbnDdo",
+  authDomain: "caregiver-coordination-hub.firebaseapp.com",
+  projectId: "caregiver-coordination-hub",
+  storageBucket: "caregiver-coordination-hub.appspot.com",
+  messagingSenderId: "450043180225",
+  appId: "1:450043180225:web:6259141b059625484236fe",
+  measurementId: "G-YEXL6ZZ4DT"
 };
+
 
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
