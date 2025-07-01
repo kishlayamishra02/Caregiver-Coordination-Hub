5ddea2e74f1e2fa0b9bac7044140da4f9e5ba95e
Kishlaya Mishra <kishlayamishra@gmail.com>
2025-07-01 09:46:03 +0530

    Setup dotenv for Google OAuth and Firebase env vars

--- caregiver-backend/gmail-auth.js
+++ caregiver-backend/gmail-auth.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
+
 const { google } = require('googleapis');
 const readline = require('readline');
 const { OAuth2 } = google.auth;
 
 // Replace with your credentials
-const CLIENT_ID = '48068951418-g6kt0nte59o8dtn3a5jllil8emgkmmn3.apps.googleusercontent.com';
-const CLIENT_SECRET = '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';
-const REDIRECT_URI = 'http://127.0.0.1:3000'; // Safer than 'urn:ietf:wg:oauth:2.0:oob'
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
 
 const oAuth2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
 
